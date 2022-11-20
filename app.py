ca59ea795a1ab70461ce92a4e0950881cf90aa60
akebaru21 <98940870+akebaru21@users.noreply.github.com>
2022-11-20 19:41:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTST
 sheet_inflow = "inflow"
 sheet_outflow = "outflow"
 url_inflow = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQWYZulM9HeQR8XmcBD9EHnS2-ZhdO8fnnHqSsPmKqh-qis_y95Ixitz0XVaMDoXm8q8b0e0Ap5xAa-/pub?gid=1924073950&single=true&output=csv&sheet={Air_Masuk}"
-url_outflow ="https://docs.google.com/spreadsheets/d/e/2PACX-1vQWYZulM9HeQR8XmcBD9EHnS2-ZhdO8fnnHqSsPmKqh-qis_y95Ixitz0XVaMDoXm8q8b0e0Ap5xAa-/pub?gid=1924073950&single=true&output=csv&sheet={Air_Keluar}"
+url_outflow ="https://docs.google.com/spreadsheets/d/e/2PACX-1vQWYZulM9HeQR8XmcBD9EHnS2-ZhdO8fnnHqSsPmKqh-qis_y95Ixitz0XVaMDoXm8q8b0e0Ap5xAa-/pubhtml?gid=1925455528&single=true&output=csv&sheet={Air_Keluar}"
 df_inflow = pd.read_csv(url_inflow)
 df_outflow = pd.read_csv(url_outflow)
 
